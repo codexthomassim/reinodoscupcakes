0b4da41facb86cf5e1256f66b7346c05ff9c9815
Thomassin <vsthomassin@gmail.com>
2025-04-17 19:27:01 -0300

    Ajuste na  área de login/cadastro

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
+//Usuário
+let userIcon = document.querySelector("#user-icon");
 //Abri carrinho
 cartIcon.onclick = () => {
   cart.classList.add("active");
@@ -10,3 +12,7 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove("active");
 };
+//Redireciona o usuário para tela de cadastro/login
+userIcon.addEventListener("click", () => {
+  window.location.href = "auth.html";
+});
